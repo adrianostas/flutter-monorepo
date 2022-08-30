8ef48217a0a045561fbcf4f8a37e6db3dfd5cf47
adrianostas <adrianostas87@gmail.com>
2022-08-30 17:06:16 +0300

    feat: fix path

--- apps/buyer_app/lib/main.dart
+++ apps/buyer_app/lib/main.dart
@@ -11,6 +11,7 @@ class BuyerApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       title: 'Buyer',
       theme: ThemeData(

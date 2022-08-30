df80ea92a8639e60dafc64c6a9a13346a8260ca3
adrianostas <adrianostas87@gmail.com>
2022-08-30 16:49:29 +0300

    feat: add print

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,7 +3,7 @@
 set -e
 VERSION="$1"
 export VERSION
-echo ls
+echo $PWD
 
 # replace version in pubspec with version received from semantic release & increment build number
 perl -i -pe 's/^(version:)(\s*)((\d+\.)+\d+)(\+)(\d+)$/$1.$2.$ENV{VERSION}.$5.($6+1)/e' "./apps/buyer_app/pubspec.yaml"

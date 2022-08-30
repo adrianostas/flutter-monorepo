c4585e780ca5cdd81e0e5b50d6236974aa98c4ce
adrianostas <adrianostas87@gmail.com>
2022-08-30 16:52:22 +0300

    feat: fix path

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -6,7 +6,7 @@ export VERSION
 echo $PWD
 
 # replace version in pubspec with version received from semantic release & increment build number
-perl -i -pe 's/^(version:)(\s*)((\d+\.)+\d+)(\+)(\d+)$/$1.$2.$ENV{VERSION}.$5.($6+1)/e' "./apps/buyer_app/pubspec.yaml"
+perl -i -pe 's/^(version:)(\s*)((\d+\.)+\d+)(\+)(\d+)$/$1.$2.$ENV{VERSION}.$5.($6+1)/e' ".flutter-monorepo//apps/buyer_app/pubspec.yaml"
 git add pubspec.yaml
 git commit -m "update_pubspec"
 git push origin master

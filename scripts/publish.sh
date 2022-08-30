1cf8c4be515d9ef2ffcf79501097dda868f01b49
adrianostas <adrianostas87@gmail.com>
2022-08-30 18:32:23 +0300

    feat: fix script path *5

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -8,7 +8,7 @@ echo current dir iss: $PWD
 cat "${PWD}/apps/buyer_app/pubspec.yaml"
 
 # replace version in pubspec with version received from semantic release & increment build number
-perl -i -pe 's/^(version:)(\s*)((\d+\.)+\d+)(\+)(\d+)$/$1.$2.$ENV{VERSION}.$5.($6+1)/e' "${PWD}/apps/buyer_app/pubspec.yaml"
+perl -i -pe 's/(version:)(\s*)((\d+\.)+\d+)(\+)(\d+)$/$1.$2.$ENV{VERSION}.$5.($6+1)/e' "${PWD}/apps/buyer_app/pubspec.yaml"
 git add pubspec.yaml
 git commit -m "update_pubspec"
 git push origin master

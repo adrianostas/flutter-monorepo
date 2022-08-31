0c2e32b546d93939580fae24d1b6f42f8eb4d6fa
adrianostas <adrianostas87@gmail.com>
2022-08-31 18:03:59 +0300

    feat: trigger ci
    BREAKING CHANGE: test

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -12,5 +12,5 @@ cider bump "$VERSION" --bump-build
 #perl -i -pe 's/(version:)(\s*)((\d+\.)+\d+)(\+)(\d+)$/$1.$2.$ENV{VERSION}.$5.($6+1)/e' pubspec.yaml
 
 git add pubspec.yaml
-git commit -m "update_pubspec"
-git push origin master
+#git commit -m "update_pubspec"
+#git push origin master

6c5d8e5f6c7825f1587651782917c66e0fbcfc96
adrianostas <adrianostas87@gmail.com>
2022-08-31 14:12:04 +0300

    feat: test without git push

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 set -e
-#VERSION="$1"
-#export VERSION
+VERSION="$1"
+export VERSION
 echo "$1"
 
-#cd apps/buyer_app
+cd apps/buyer_app
 
 # replace version in pubspec with version received from semantic release & increment build number
-#perl -i -pe 's/(version:)(\s*)((\d+\.)+\d+)(\+)(\d+)$/$1.$2.$ENV{VERSION}.$5.($6+1)/e' pubspec.yaml
+perl -i -pe 's/(version:)(\s*)((\d+\.)+\d+)(\+)(\d+)$/$1.$2.$ENV{VERSION}.$5.($6+1)/e' pubspec.yaml
 
-#git add pubspec.yaml
+git add pubspec.yaml
 #git commit -m "update_pubspec"
 #git push origin master

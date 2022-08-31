06d9b1482df14179d2c5c002af1ec0566480a691
adrianostas <adrianostas87@gmail.com>
2022-08-31 14:51:31 +0300

    feat: add cider to script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -5,10 +5,15 @@ VERSION="$1"
 export VERSION
 echo "$1"
 
+export PATH="$PATH":"$HOME/.pub-cache/bin"
+pub global activate cider
+
 cd apps/buyer_app
 
+cider bump VERSION
+
 # replace version in pubspec with version received from semantic release & increment build number
-perl -i -pe 's/(version:)(\s*)((\d+\.)+\d+)(\+)(\d+)$/$1.$2.$ENV{VERSION}.$5.($6+1)/e' pubspec.yaml
+#perl -i -pe 's/(version:)(\s*)((\d+\.)+\d+)(\+)(\d+)$/$1.$2.$ENV{VERSION}.$5.($6+1)/e' pubspec.yaml
 
 git add pubspec.yaml
 git commit -m "update_pubspec"

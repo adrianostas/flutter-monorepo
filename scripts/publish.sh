6d715c9f9694af2ed6567eb7534dc695aa9728fb
adrianostas <adrianostas87@gmail.com>
2022-08-31 11:08:35 +0300

    feat: test melos

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,11 +4,11 @@ set -e
 VERSION="$1"
 export VERSION
 
-cd apps/buyer_app
+#cd apps/buyer_app
 
 # replace version in pubspec with version received from semantic release & increment build number
-perl -i -pe 's/(version:)(\s*)((\d+\.)+\d+)(\+)(\d+)$/$1.$2.$ENV{VERSION}.$5.($6+1)/e' pubspec.yaml
+#perl -i -pe 's/(version:)(\s*)((\d+\.)+\d+)(\+)(\d+)$/$1.$2.$ENV{VERSION}.$5.($6+1)/e' pubspec.yaml
+melos version -buyer_app:VERSION
 git add pubspec.yaml
 git commit -m "update_pubspec"
 git push origin master
-

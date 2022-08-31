f0dfca4c014300ed37f91076dcd36233f6e035b8
adrianostas <adrianostas87@gmail.com>
2022-08-31 16:41:49 +0300

    feat: remove job dep

--- apps/buyer_app/lib/main.dart
+++ apps/buyer_app/lib/main.dart
@@ -23,6 +23,7 @@ class MyHomePage extends StatelessWidget {
   final String title;
   MyHomePage({Key key, this.title}) : super(key: key);
 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(

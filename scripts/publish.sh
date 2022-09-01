c81190d62b69276db75540172bd2f3677821a667
adrianostas <adrianostas87@gmail.com>
2022-09-01 14:07:59 +0300

    fix: print exit code

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,11 +4,13 @@ set -e
 VERSION="$1"
 export VERSION
 echo "$VERSION"
+# shellcheck disable=SC2154
+echo "$e"
 cd apps/buyer_app
 
 cider bump "$VERSION" --bump-build
 
-
-#git add pubspec.yaml
+echo $?
+git add pubspec.yaml
 #git commit -m "update_pubspec"
 #git push origin master

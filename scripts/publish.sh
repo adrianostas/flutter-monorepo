210c8d47253118225130f2cb517b65a12ae8d18c
adrianostas <adrianostas87@gmail.com>
2022-09-01 12:10:29 +0300

    feat: change version with melos *3

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,10 +4,10 @@ set -e
 VERSION="$1"
 export VERSION
 echo "$VERSION"
-#cd apps/buyer_app
+cd apps/buyer_app
 
-#cider bump "$VERSION" --bump-build
-melos version buyer_app "$VERSION"
+cider bump "$VERSION" --bump-build
+
 
 #git add pubspec.yaml
 git commit -m "update_pubspec"
